706cbfa9bf2a92be91a173ddb6f14d4095fff2f1
peoplewhoplay <42812456+peoplewhoplay@users.noreply.github.com>
2018-09-04 10:05:46 +0700

    Update tricky-alchemy.cpp

--- Week01 - Intro/tricky-alchemy.cpp
+++ Week01 - Intro/tricky-alchemy.cpp
@@ -12,10 +12,10 @@ int main()
     long long green_BALLS = 0;          //Declare green balls
     long long blue_BALLS = 0;           //Declare blue balls
 
-    long long remaining = 0;            //How much crystals Grisha needs at the end
-
     long long ballcounter1 = 0;         //Yellow Crystals needed
     long long ballcounter2 = 0;         //Blue Crystals needed
+
+    long long remaining = 0;            //Remaining Crystals (if needed)
 
     cin >> yellow_CRYSTALS >> blue_CRYSTALS;          //Input amount of crystals Grisha possesses
     cin >> yellow_BALLS >> green_BALLS >> blue_BALLS;  // Input amount of balls Grisha wants to make
@@ -23,14 +23,13 @@ int main()
     if (yellow_CRYSTALS < (2*yellow_BALLS + green_BALLS))      //Check if the amount of yellow crystals is insufficient to fulfill amount of yellow and green balls to be made
     {
         ballcounter1 += (2*yellow_BALLS + green_BALLS) - yellow_CRYSTALS;          //Find remaining yellow crystals required
-        remaining += ballcounter1;          //Add to shopping list
     }
     if (blue_CRYSTALS < (3*blue_BALLS + green_BALLS))          //Check if the amount of blue crystals in insufficient to fulfill amount of blue and green balls to be made
     {
         ballcounter2 = (3*blue_BALLS + green_BALLS) - blue_CRYSTALS;       //Find remaining blue crystals required
-        remaining +=  ballcounter2;         //Add to shopping list
     }
-    cout << remaining;          //Print remaining crystals Grisha has to acquire
+    remaining = ballcounter1 + ballcounter2;        //Sum of missing yellow and blue crystals
+    cout << remaining;         //Print remaining crystals Grisha has to acquire
 
     return 0;
 }
