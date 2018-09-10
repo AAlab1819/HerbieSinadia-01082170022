e20495718e576962233d58c16e8e0f368b8e13d1
Mystery Man <42812456+peoplewhoplay@users.noreply.github.com>
2018-09-10 19:43:23 +0700

    Update spyke-talks.cpp

--- Week02-Sorting/spyke-talks.cpp
+++ Week02-Sorting/spyke-talks.cpp
@@ -1,82 +1,16 @@
 #include <iostream>
 using namespace std;
 
-//Function to merge back array after splitting them in fuction "mergesort"
-void mergeback (int *arr, int low, int high, int mid)
-{
-    int a;
-    int b;
-    int c;
-    int d[high-low+1];          //Temporary array
-
-    a = low;            //Lower part
-    c = 0;              //Index for checking
-    b = mid+1;          //Higher part
-
-    //Merge the two parts to the temporary array d[]
-    while (a <= mid && b <= high)
-    {
-        if (arr[a] < arr[b])
-        {
-            d[c] = arr[a];
-            c++;
-            a++;
-        }
-        else
-        {
-            d[c] = arr[b];
-            c++;
-            b++;
-        }
-    }
-
-    //Insert values from i to mid into d[]
-    while (a <= mid)
-    {
-        d[c] = arr[a];
-        c++;
-        a++;
-    }
-
-    //Insert values from mid to high into d[]
-    while (b <= high)
-    {
-        d[c] = arr[b];
-        c++;
-        b++;
-    }
-
-    //Construct sorted array based on d[]
-    for (a = low; a <= high; a++)
-    {
-        arr[a] = d[a-low];
-    }
-}
-
-//Function to split arrays (tree)
-void mergesort(int *arr, int low, int high)
-{
-    int mid;
-    if (low < high)
-    {
-        mid = (low+high)/2;                 //Declare middle part (will be split)
-
-        //Split the array until there is no more to be split
-        mergesort(arr, low, mid);
-        mergesort(arr, mid+1, high);
-
-        
-        mergeback(arr, low, high, mid);         //Merge the array back
-    }
-}
-
-//Main function
 int main()
 {
     int secretaries;
 
     int currentcheck = 0;
     int validsessions = 0;
+    
+    int temp;
+    int num;
+    int minimum;
 
     cin >> secretaries;                 //Input amount of secretaries
 
@@ -87,11 +21,25 @@ int main()
     {
         cin >> callSession[a];          //Input call sessions
     }
+    
+     for (int f = 0; f < secretaries-1; f++)
+    {
+        minimum = callSession[f];
+        num = f;
 
-    mergesort(callSession,0,secretaries-1);             //Sort the array using merge sort
-
-    cout << endl;
-
+        for (int g = f+1; g < secretaries; g++)
+        {
+            if (minimum > callSession[g])
+            {
+                minimum = callSession[g];
+                num = g;
+            }
+        }
+            temp = callSession[f];
+            callSession[f] = callSession[num];
+            callSession[num] = temp;
+    }
+    
     //Check for valid call sessions
     for (int i = 0; i < secretaries-1; i++)
     {
@@ -117,5 +65,6 @@ int main()
         }
         //else keep going
     }
-    cout << validsessions;              //Print results
+    cout << validsessions; //Print results
+    
 }
